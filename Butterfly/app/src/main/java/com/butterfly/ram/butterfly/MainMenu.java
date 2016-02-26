2e5c310237b8e6161bc29c04af18bda0001200cf
Rostislav Aliferovich <rostislawk@yandex-team.ru>
2016-02-26 16:34:14 +0300

    configure drawer menu items

--- Butterfly/app/src/main/java/com/butterfly/ram/butterfly/MainMenu.java
+++ Butterfly/app/src/main/java/com/butterfly/ram/butterfly/MainMenu.java
@@ -80,17 +80,15 @@ public class MainMenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_photos) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_take_photo) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_favorites) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
